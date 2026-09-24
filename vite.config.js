cd4482daced8ac4288506201e7a08bddfa796e94
poiu0825-aibot <poiu0825@gmail.com>
2026-09-24 15:05:26 +0800

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: { proxy: { '/bgio': { target: 'http://localhost:8000', changeOrigin: true, ws: true } } },
+  build: { rollupOptions: { output: { manualChunks: { three: ['three'], boardgame: ['boardgame.io'] } } } }
+});
+
